e37d80bd83977e9c935fa6fd3279b17acdcd126e
Jamesyoub <jamesrayoub@gmail.com>
2021-07-01 16:13:25 -0400

    cast FK to int

--- models/stg_sitetracker__site.sql
+++ models/stg_sitetracker__site.sql
@@ -14,7 +14,7 @@ with base as (
         _fivetran_synced,
         contact_c as contact_id,
         created_date as created_at,
-        hub_spot_deal_id_c as deal_id,
+        cast(hub_spot_deal_id_c as int64) as deal_id,
         last_modified_date as last_modified_at,
         name as display_name,
         sitetracker_territory_c as territory_id
